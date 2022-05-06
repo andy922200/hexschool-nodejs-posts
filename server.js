5fc36f6832796e700c63be85dc3105704a5e2f01
andy922200-m1 <andy922200@gmail.com>
2022-05-06 15:01:58 +0800

    1. fix port config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 
 /* require config */
 require('dotenv').config()
